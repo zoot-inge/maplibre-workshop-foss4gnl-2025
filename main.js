06da019a830fe9bda09cbc26d3c0e3c0890e8b10
Inge La Rivière <3697652+zoot-inge@users.noreply.github.com>
2025-07-02 09:52:35 +0000

    deel 5

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";//"https://esm
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    //style: 'https://demotiles.maplibre.org/style.json', // style URL
+    //center: [0, 0], // starting position [lng, lat]
+    //zoom: 1 // starting zoom
+    style: './assets/style.json',
+    center: [51.96857, 5.66509], // starting position [lng, lat]
+    zoom: 13 // starting zoom
 });
+
+import { Protocol } from "https://esm.sh/pmtiles";
+const protocol = new Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
